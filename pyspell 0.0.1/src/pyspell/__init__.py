1ffbae957a0f58e90e1cfc17d6cf0e0a855995e0
made-up <132244922+vertmit@users.noreply.github.com>
2024-01-31 17:15:13 +1300

    Add files via upload

---
+++ pyspell 0.0.1/src/pyspell/__init__.py
@@ -0,0 +1,6 @@
+from .spellcheck import load_dictionary
+from .spellcheck import spell_check
+from .spellcheck import correct_word
+from .spellcheck import correct_sentance
+
+__all__ = ['load_dictionary', 'spell_check', 'correct_word', 'correct_sentance']
