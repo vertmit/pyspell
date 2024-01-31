01cadbf77b259f8eebd1e8f358e25ca1317f180c
made-up <132244922+vertmit@users.noreply.github.com>
2024-01-31 17:16:50 +1300

    Delete pyspell 0.0.1/src directory

--- pyspell 0.0.1/src/pyspell/__init__.py
+++
@@ -1,6 +0,0 @@
-from .spellcheck import load_dictionary
-from .spellcheck import spell_check
-from .spellcheck import correct_word
-from .spellcheck import correct_sentance
-
-__all__ = ['load_dictionary', 'spell_check', 'correct_word', 'correct_sentance']
